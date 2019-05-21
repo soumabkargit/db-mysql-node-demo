cd4e217376b8b188f1afe2d66beab6cfd36bfa94
Abdoul-Karim SOUMAHORO <soumabkar@gmail.com>
2019-05-21 11:29:19 +0200

    étape 1

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ var mysql      = require('mysql');
 var pool = mysql.createPool({
   host     : 'localhost',
   user     : 'root',
-  password : '',
-  database : 'node-demo'
+  password : 'root',
+  database : 'node_demo'
 });
 
 pool.getConnection(function (err, connection) {
-    if (err) throw err; 
-    var query1 = 'SELECT * FROM`node-demo`.`personne`;';
+    if (err) throw err;
+
+
+
+    var query1 = 'SELECT * FROM`node_demo`.`personne`;';
     connection.query(query1, function (error, results, fields) {
-        connection.release();
+        console.log(results);
+        console.log(fields);
         if (error) throw error;
     });
 
@@ -18,11 +22,27 @@ pool.getConnection(function (err, connection) {
     var post = { nom: 'Soum', prenom: 'AbKar' };
     var query2 = 'INSERT INTO `personne` SET ?';
     connection.query(query2, post, function (error, results, fields) {
-        connection.release();
+        console.log(results);
+        console.log(fields);
         if (error) throw error;
     });
 
 
+    var query3 = `update personne as p  set p.nom= `+ connection.escape('Soumabkar') + `where p.id=`+ connection.escape(7) ;
+    connection.query(query3, function (error, results, fields) {
+        console.log(results);
+        console.log(fields);
+        if (error) throw error;
+    });
+
+
+    var query4 = 'delete from node_demo.personne as p where p.id = 7';
+    connection.query(query4, function (error, results, fields) {
+        console.log(results);
+        console.log(fields);
+        if (error) throw error;
+    });
+    connection.release();
 });
 
 pool.on('acquire', function (connection) {
